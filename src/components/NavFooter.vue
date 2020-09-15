a8fc9723bab7fd5fd691431d1355a9ae22692b82
zxb <1655244880@qq.com>
2020-09-15 12:37:54 +0800

    first commit

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>             
+    <div class="footer-link">
+      <a href="javascript:;" target="_blank">小米商城主页</a><span>|</span>
+      <a href="javascript:;" target="_blank">小米之家</a><span>|</span>
+      <a href="javascript:;" target="_blank">关于小米</a><span>|</span>
+      <a href="javascript:;" target="_blank">联系我们</a><span>|</span>
+      <a href="javascript:;" target="_blank">特色服务</a>
+    </div>
+    <div class="copyright">Copyright ©2020 <span class="domain">mi.jzzz66.cn</span> All Rights Reserved.</div>
+  </div>
+</template>
+<script>
+  export default{
+    name:'nav-footer'
+  }
+</script>
+<style lang="scss" scoped>
+  .footer{
+    height:234px;
+    border-top:4px solid #FF6600;
+    background-color:#333333;
+    color:#999999;
+    font-size:16px;
+    text-align:center;
+    .footer-logo{
+      margin-top:46px;
+      margin-bottom:31px;
+      img{
+        width:53px;
+        height:36px;
+        margin-bottom:13px;
+      }
+    }
+    .footer-link{
+      a{
+        color:#999999;
+        display:inline-block;
+      }
+      span{
+        margin:0 10px;
+      }
+      margin-bottom:13px;
+    }
+    .copyright{
+      .domain{
+        color:#FF6600;
+      }
+    }
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueLazyLoad from 'vue-lazyload'
+import VueCookie from 'vue-cookie'
+import { Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import App from './App.vue'
+
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; 
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = '/#/login';
+    return Promise.reject(res);
+  }else{
+    Message.warning(res.msg);
+    return Promise.reject(res);
+  }
+},(error)=>{
+  let res = error.response;
+  Message.error(res.data.message);
+  return Promise.reject(error);
+});
+
+Vue.use(VueAxios,axios);
+Vue.use(VueCookie);
+Vue.use(VueLazyLoad,{
+  loading:'/imgs/loading-svg/loading-bars.svg'
+})
+Vue.prototype.$message = Message;
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  router,
+  render: h => h(App),
+}).$mount('#app')
